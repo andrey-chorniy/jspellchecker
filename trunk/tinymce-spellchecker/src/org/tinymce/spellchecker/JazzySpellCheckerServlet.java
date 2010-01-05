200e8f76f1c27b2f5c3ff85d2ea9cf4c3634aa9a
andrey_chorniy <andrey_chorniy@58ebb0c1-2a14-49b6-8940-e151352903fa>
2010-01-05 21:01:37 +0000

    1) Code refactored to support different implemntations of spell-checkers. new class TinyMCESpellCheckerServlet define request reading, delegate spell-checking to abstract methods and write response in JSON 2) JMySpell spellchecker implementation has been added 3) servlet paths changed to (please update tiny_mce config scripts)         <url-pattern>/jazzy-spellchecker</url-pattern>         <url-pattern>/google-spellchecker</url-pattern> +       <url-pattern>/jmyspell-spellchecker</url-pattern>

--- trunk/tinymce-spellchecker/src/org/tinymce/spellchecker/JazzySpellCheckerServlet.java
+++ trunk/tinymce-spellchecker/src/org/tinymce/spellchecker/JazzySpellCheckerServlet.java
@@ -34,223 +34,45 @@ package org.tinymce.spellchecker;
 
 import com.swabunga.spell.engine.Configuration;
 import com.swabunga.spell.engine.SpellDictionaryHashMap;
+import com.swabunga.spell.engine.Word;
 import com.swabunga.spell.event.SpellChecker;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.*;
 import java.util.*;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class JazzySpellCheckerServlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
+public class JazzySpellCheckerServlet extends TinyMCESpellCheckerServlet {
 
     private static Logger logger = Logger.getLogger(JazzySpellCheckerServlet.class.getName());
-
-    private static final String MAX_SUGGESTIONS_COUNT_PARAM = "maxSuggestionsCount";
-    private static final String PRELOADED_LANGUAGES_PARAM = "preloadedLanguages";
-
-    private static final String DEFAULT_LANGUAGE = "en";
-    private static final String GET_METHOD_RESPONSE_ERROR = "This servlet expects a JSON encoded body, POSTed to this URL";
-
-    private static String DEFAULT_PRELOADED_LANGUAGES = "en-us;es";
-
-    private enum methods {
-        checkWords, getSuggestions
-    }
-
-    private int maxSuggestionsCount = 25;
+    private static final long serialVersionUID = 2L;
 
     private Map<String, SpellChecker> spellcheckersCache = new Hashtable<String, SpellChecker>(2);
 
-    @Override
-    public void init() throws ServletException {
-        super.init();
-        preloadSpellcheckers();
-        readMaxSuggestionsCount();
+    protected void preloadLanguageChecker(String preloadedLanguage) throws SpellCheckException {
+        getChecker(preloadedLanguage);
     }
 
-    private void preloadSpellcheckers() throws ServletException {
-        String preloaded = getServletConfig().getInitParameter(PRELOADED_LANGUAGES_PARAM);
-        if (preloaded == null || preloaded.trim().length() == 0) {
-            preloaded = DEFAULT_PRELOADED_LANGUAGES;
-        }
-
-        for (String preloadedLanguage : preloaded.split(";")) {
-            try {
-                getChecker(preloadedLanguage);
-            } catch (SpellCheckException e) {
-                //wrong servlet configuration
-                throw new ServletException(e);
+    protected List<String> findMisspelledWords(Iterator<String> checkedWordsIterator,
+                                               String lang) throws SpellCheckException {
+        List<String> misspelledWordsList = new ArrayList<String>();
+        SpellChecker checker = (SpellChecker) getChecker(lang);
+        while (checkedWordsIterator.hasNext()) {
+            String word = checkedWordsIterator.next();
+            if (!word.equals("") && !checker.isCorrect(word) && !checker.isCorrect(word.toLowerCase())) {
+                misspelledWordsList.add(word);
             }
         }
+        return misspelledWordsList;
     }
 
-    private void readMaxSuggestionsCount() throws ServletException {
-        String suggestionsCountParam = getServletConfig().getInitParameter(MAX_SUGGESTIONS_COUNT_PARAM);
-        if (suggestionsCountParam != null && suggestionsCountParam.trim().length() > 0) {
-            try {
-                maxSuggestionsCount = Integer.parseInt(suggestionsCountParam.trim());
-            } catch (NumberFormatException ex) {
-                //wrong servlet configuration, possibly a typo
-                throw new ServletException(ex);
-            }
+    protected List<String> findSuggestions(String word, String lang, int maxSuggestions) throws SpellCheckException {
+        List<String> suggestionsList = new ArrayList<String>(maxSuggestions);
+        SpellChecker checker = (SpellChecker) getChecker(lang);
+        ListIterator<Word> suggestionsIt = checker.getSuggestions(word, maxSuggestions).listIterator();
+        while (suggestionsIt.hasNext()) {
+            suggestionsList.add(suggestionsIt.next().getWord());
         }
-    }
-
-
-    /**
-     * @see javax.servlet.Servlet#destroy()
-     */
-    public void destroy() {
-        super.destroy();
-        //remove unused objects from memory
-        spellcheckersCache.clear();
-        spellcheckersCache = new Hashtable<String, SpellChecker>();
-    }
-
-    /**
-     * GET method is not supported
-     *
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-     *      response)
-     */
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        setResponeHeaders(response);
-        PrintWriter pw = response.getWriter();
-        pw.println(GET_METHOD_RESPONSE_ERROR);
-    }
-
-    /**
-     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-     *      response)
-     */
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        setResponeHeaders(response);
-        try {
-            JSONObject jsonInput = readRequest(request);
-
-            String methodName = jsonInput.optString("method");
-            if (methodName == null || methodName.trim().equals("")) {
-                throw new SpellCheckException("Wrong spellchecker-method-name:" + methodName);
-            }
-
-            JSONObject jsonOutput = new JSONObject("{'id':null,'result':[],'error':null}");
-            switch (methods.valueOf(methodName.trim())) {
-                case checkWords:
-                    jsonOutput.put("result", checkWords(jsonInput.optJSONArray("params")));
-                    break;
-                case getSuggestions:
-                    jsonOutput.put("result", getSuggestions(jsonInput.optJSONArray("params")));
-                    break;
-                default:
-                    throw new SpellCheckException("Unimplemented spellchecker method {" + methodName + "}");
-            }
-
-            PrintWriter pw = response.getWriter();
-            pw.println(jsonOutput.toString());
-
-        } catch (SpellCheckException se) {
-            logger.log(Level.WARNING, se.getMessage(), se);
-            returnError(response, se.getMessage());
-        } catch (Exception e) {
-            logger.log(Level.WARNING, e.getMessage(), e);
-            returnError(response, e.getMessage());
-        }
-
-        response.getWriter().flush();
-    }
-
-    private JSONObject readRequest(HttpServletRequest request) throws SpellCheckException {
-        JSONObject jsonInput = null;
-        try {
-            jsonInput = new JSONObject(getRequestBody(request));
-        } catch (JSONException e) {
-            throw new SpellCheckException("Could not interpret JSON request body", e);
-        } catch (IOException e) {
-            throw new SpellCheckException("Error reading request body", e);
-        }
-        return jsonInput;
-    }
-
-    /**
-     * @param response
-     */
-    private void setResponeHeaders(HttpServletResponse response) {
-        response.setContentType("text/plain; charset=utf-8");
-        response.setCharacterEncoding("utf-8");
-        response.setHeader("Cache-Control", "no-store, no-cache");
-        response.setHeader("Pragma", "no-cache");
-        response.setDateHeader("Expires", System.currentTimeMillis());
-    }
-
-
-    /**
-     * @param request
-     * @return read the request content and return it as String object
-     * @throws IOException
-     */
-    private String getRequestBody(HttpServletRequest request) throws IOException {
-        StringBuilder sb = new StringBuilder();
-        String line = request.getReader().readLine();
-        while (null != line) {
-            sb.append(line);
-            line = request.getReader().readLine();
-        }
-        return sb.toString();
-    }
-
-    /**
-     * @param response
-     * @param message
-     */
-    private void returnError(HttpServletResponse response, String message) {
-        response.setStatus(500);
-        try {
-            response.getWriter().print("{'id':null,'response':[],'error':'" + message + "'}");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private JSONArray checkWords(JSONArray params) throws SpellCheckException {
-        JSONArray misspelled = new JSONArray();
-        if (params != null) {
-            JSONArray words = params.optJSONArray(1);
-            String lang = params.optString(0);
-            lang = ("".equals(lang)) ? DEFAULT_LANGUAGE : lang;
-            SpellChecker checker = getChecker(lang);
-            for (int i = 0; i < words.length(); i++) {
-                String word = words.optString(i);
-                if (!word.equals("") && !checker.isCorrect(word) && !checker.isCorrect(word.toLowerCase())) {
-                    misspelled.put(word);
-                }
-            }
-        }
-        return misspelled;
-    }
-
-    private JSONArray getSuggestions(JSONArray params) throws SpellCheckException {
-        JSONArray suggestions = new JSONArray();
-        if (params != null) {
-            String lang = params.optString(0);
-            lang = ("".equals(lang)) ? DEFAULT_LANGUAGE : lang;
-            String word = params.optString(1);
-            SpellChecker checker = getChecker(lang);
-            ListIterator<?> suggestionsIt = checker.getSuggestions(word, maxSuggestionsCount).listIterator();
-            while (suggestionsIt.hasNext()) {
-                suggestions.put(suggestionsIt.next());
-            }
-        }
-        return suggestions;
+        return suggestionsList;
     }
 
     /**
@@ -263,8 +85,7 @@ public class JazzySpellCheckerServlet extends HttpServlet {
      * @throws SpellCheckException if method failed to load the SpellChecker for lang (it happens if there is no
      *                             dictionaries for that language was found in the classpath
      */
-    private SpellChecker getChecker(String lang) throws SpellCheckException {
-
+    protected Object getChecker(String lang) throws SpellCheckException {
         SpellChecker cachedCheker = spellcheckersCache.get(lang);
         if (cachedCheker == null) {
             cachedCheker = loadSpellChecker(lang);
@@ -275,7 +96,7 @@ public class JazzySpellCheckerServlet extends HttpServlet {
 
     /**
      * Load the SpellChecker object form the file-system.
-     * TODO: It possibly worth to rework it to load from class-path, since current implementation assumes that WAR is exploeded which is not always can be the case
+     * TODO: It possibly worth to rework it to load from class-path, since current implementation assumes that WAR is exploded which is not always can be the case
      *
      * @param lang
      * @return loaded SpellChecker object
@@ -298,7 +119,7 @@ public class JazzySpellCheckerServlet extends HttpServlet {
      */
 
     protected List<File> getDictionaryFiles(String language) throws SpellCheckException {
-        String pathToDictionaries = getServletContext().getRealPath("/WEB-INF/dictionary");
+        String pathToDictionaries = getServletContext().getRealPath("/WEB-INF/dictionary/jazzy");
         File dictionariesDir = new File(pathToDictionaries);
         List<File> langDictionaries = getDictionaryFiles(language, dictionariesDir, language);
         if (langDictionaries.size() == 0) {
@@ -345,9 +166,9 @@ public class JazzySpellCheckerServlet extends HttpServlet {
                                           final String prefix) throws SpellCheckException {
         File languageDictionary = new File(dictionariesDir, lang);
         File[] languageDictionaries = languageDictionary.listFiles(new FileFilter() {
-            public boolean accept(File pathname) {
-                if (pathname.isFile()) {
-                    return pathname.getName().startsWith(prefix);
+            public boolean accept(File pathName) {
+                if (pathName.isFile()) {
+                    return pathName.getName().startsWith(prefix);
                 }
                 return false;
             }
@@ -360,5 +181,9 @@ public class JazzySpellCheckerServlet extends HttpServlet {
         return dicitonaries;
     }
 
-
+    @Override
+    protected void clearSpellcheckerCache() {
+        spellcheckersCache.clear();
+        spellcheckersCache = new Hashtable<String, SpellChecker>();
+    }
 }
